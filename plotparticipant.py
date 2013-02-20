736258d65784e077dabd3d35c58bb04d58f4333d
gniezen <gerrit.niezen@gmail.com>
2013-02-20 09:19:55 +0000

    Graphs plotted for all participants.

--- plotparticipant.py
+++ plotparticipant.py
@@ -1,24 +1,20 @@
 import json
+import matplotlib
 import matplotlib.pyplot as p
+from matplotlib.backends.backend_pdf import PdfPages
 import pickle
 import itertools
 
 import operator
 
-
-
 time=0
-reference = 6.8 
 yDisplay = []
 yOutput = [0]
 yVelocity = [0]
 x = [0]
 
-#def most_common(L):
-#  groups = itertools.groupby(sorted(L))
-#  def _auxfun((item, iterable)):
-#    return len(list(iterable)), -L.index(item)
-#  return max(groups, key=_auxfun)[0]
+font = {'size' : 5}
+matplotlib.rc('font', **font)
 
 def getData():
     users = []
@@ -31,60 +27,86 @@ def getData():
         print x['name']
         results = []
         for i in x['chevrons']:
-            results.append((i['st'],i['ev'],i['rt']))
+            
+            mouseEvents = 0
+            for val in i['rt']:
+                if "mouse" in val['im']:
+                    mouseEvents +=1
+            print mouseEvents
+            if mouseEvents % 2 == 0:
+                print "Equal, adding.."
+                results.append((i['st'],i['ev'],i['rt']))
+            else:
+                print "Not equal, discarding.."
+
         users.append(results)
 
     return users
 
 
-user = getData()[2] # get first user
 
-nrPlots = len(user)
-print nrPlots
+users = getData()
 
-iterations = 0
-for y in range(0,10):
-#for result in user:
-    result = user[y]    
-    p.subplot(5,2,y)
-    
-    #p.subplot(nrPlots/2,2,iterations)
-    #iterations += 1
+for iteruser in range(0,len(users)):
 
-    time = result[0] # take start time as t:=0
-    reference = result[1]
+    user = users[iteruser] 
+    pp = PdfPages('pplots/participant'+str(iteruser)+'.pdf')
 
-    yDisplay = []
-    x = [0]
-    mouseEvents = 0
+    nrPlots = len(user)
+    print "Number of plots: ", nrPlots
 
-    for i in result[2]:
-        #print i['btn'], ": ", i['im'], ": ",i['v'],": ", i['ts'] - time
-        if "mouse" in i['im']:
-            mouseEvents +=1
-        
-        if i['ts'] - time != 0:    
-            x.append(i['ts'] - time)
-
-        yDisplay.append(i['v'])
-
-    print mouseEvents
-    if mouseEvents % 2 == 0:
-        print "EQUAL"
+    if(nrPlots == 100):
+        iterz = 6
     else:
-        print "NOT EQUAL"
-    
+        iterz = (nrPlots/20)+2
 
-    p.plot(x,yDisplay,'r+-', label="User")
-    p.plot(x,[reference for i in x],'b', label = "Reference signal")
+    iteration= -1
 
-p.xlabel("Time")
-#p.legend()
-p.tight_layout() # to prevent clipping of titles and labels         
-#p.savefig('participant.pdf',format="pdf") #Only valid for (e)ps:,papertype='A4', orientation='landscape')
-p.show()
-p.close()
+    for z in range(1,iterz):
+        p.clf()
+        for y in range(0,20):
+            iteration +=1
 
+            if iteration >= len(user):
+                break
 
+            result = user[iteration]    
+            p.subplot(5,4,y)
+            
+
+            time = result[0] # take start time as t:=0
+            reference = result[1]
+
+            yDisplay = []
+            x = [0]
+
+            for i in result[2]:
+                #print i['btn'], ": ", i['im'], ": ",i['v'],": ", i['ts'] - time
+                
+                if i['ts'] - time != 0:    
+                    x.append(i['ts'] - time)
+
+                yDisplay.append(i['v'])
+
+            
+            p.title("User "+ str(iteruser)+ ", target="+str(result[1]),fontsize=6)
+            
+            try:
+                p.plot(x,yDisplay,'r+-', label="User")
+            except ValueError, e:
+                p.text(0.5, 0.5,'Unable to plot',horizontalalignment='center',verticalalignment='center')
+                print e
+
+            p.plot(x,[reference for i in x],'b', label = "Reference signal")
+        
+        p.tight_layout() # to prevent clipping of titles and labels
+
+        figure = p.gcf() # get current figure
+        figure.set_size_inches(11.69, 8.27)
+        figure.savefig(pp,format="pdf",papertype='a4',dpi=100)
+        #p.show()
+        p.close()
+
+    pp.close()
 
 
